7cb74bf847d8ac35e9811a4224f5f1c4d98e058f
TheLolik <79652963+TheLolik@users.noreply.github.com>
2022-05-20 21:30:20 +0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+local function load(file,lib,win)
+  local loaded = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheLolik/sb-script/main/"..file..".lua"))()
+  loaded:start(lib,win)
+end
+
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/TheLolik/vape-lite-update/main/lib.lua")()
+local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+load("swords",lib,win)
+wait()
+load("players",lib,win)
+wait()
+load("other",lib,win)
