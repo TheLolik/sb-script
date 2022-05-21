e95421015837e3c04d8d80921e6fc384bd0e7fcd
TheLolik <79652963+TheLolik@users.noreply.github.com>
2022-05-21 15:12:56 +0400

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -1,20 +1,23 @@
 local f = {}
-local function f:fire(args,tool)
+local function f:fire(args,str,tool)
+    if str == false then
+        args = unpack(args)
+    end
     if game.Players.LocalPlayer.Character then
         if tool == nil then
             if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
-                return game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool").RemoteEvent:FireServer(unpack(args))
+                return game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool").RemoteEvent:FireServer(args)
             elseif game.Players.LocalPlayer.Backpack:FindFirstChildOfClass("Tool") then
-                return game:GetService("Players").LocalPlayer.Backpack:FindFirstChildOfClass("Tool").RemoteEvent:FireServer(unpack(args))
+                return game:GetService("Players").LocalPlayer.Backpack:FindFirstChildOfClass("Tool").RemoteEvent:FireServer(args)
             end
         else
             if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
                 if game.Players.LocalPlayer.Character:FindFirstChild(tool) then
-                    return game:GetService("Players").LocalPlayer.Character:FindFirstChild(tool).RemoteEvent:FireServer(unpack(args))
+                    return game:GetService("Players").LocalPlayer.Character:FindFirstChild(tool).RemoteEvent:FireServer(args)
                 end
             elseif game.Players.LocalPlayer.Backpack:FindFirstChildOfClass("Tool") then
                 if game.Players.LocalPlayer.Backpack:FindFirstChild(tool) then
-                    return game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(tool).RemoteEvent:FireServer(unpack(args))
+                    return game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(tool).RemoteEvent:FireServer(args)
                 end
             end
         end
