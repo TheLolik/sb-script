fa27f505cefe602e952b31d4b9ffe4aee4b8b4bb
TheLolik <79652963+TheLolik@users.noreply.github.com>
2022-05-23 11:43:31 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 -- Do not copy or skid scripts from this repo without giving credits to pix_dev
+-- Owner of game sucks my d##k
 local function load(file,lib,win)
   local loaded = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheLolik/sb-script/main/"..file..".lua"))()
   loaded:start(lib,win)
