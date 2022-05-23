0f107dcec4b2f2512483218f3b66dadf237bb443
TheLolik <79652963+TheLolik@users.noreply.github.com>
2022-05-23 16:33:08 +0400

    Update patched.lua

--- patched.lua
+++ patched.lua
@@ -1,6 +1,6 @@
 -- Theres only patched functions which can make you get banned
 
-tab:Toggle("Loop-use sword ability.",false, function(t)
+tab:Toggle("Loop-use sword ability.",false, function(t) -- semi-patched
     lsa = t
     while lsa == true and game.RunService.RenderStepped:Wait() do
         f:fire("ability")
